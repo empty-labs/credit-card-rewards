64496ae116796b76dcce933270b320e6bba01cf9
mdt2014 <mdt2014@gmail.com>
2025-12-30 20:45:41 -0500

    Added simple view in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ st.title("💳 Credit Card Rewards Explorer")
 
 # Prep card options
 data = ru.prep_benefits_data()
-best_by_category = ru.determine_best_by_category(data)
 
 card_names = list(data["Card"])
 
@@ -23,3 +22,14 @@ selected_card = st.selectbox(
     "Select a credit card:",
     card_names
 )
+
+# Run simulation
+run_button = st.button("Run Credit Card Rewards Explorer")
+
+if run_button:
+    st.subheader("Credit Card Rewards Results")
+
+    with st.spinner("Simulating tournament..."):
+        rewards = ru.determine_best_by_category(data)
+
+    st.dataframe(rewards, width='content')
